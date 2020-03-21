d4cad3a0f43a686f30e2311877be0dbb534bf2fc
yennycheung <yennycheung@hotmail.com>
2020-03-21 23:49:52 +0100

    Changes header background and style adjustment

--- danceagainstcorona/src/Header.js
+++ danceagainstcorona/src/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import LisLogo from './lisalone.svg';
 
 import './Header.css';
 
 function Header() {
 	return (
 		<div className="header">
-			<div class="textwrap">
-				<h1>dance</h1>
-				<h1>against</h1>
-				<h1>corona</h1>
-			</div>
+			{[...Array(200)].map((e, i) => <img src={LisLogo} alt="LisLogo"/>)}
+			<a href="https://www.instagram.com/danceagainstcorona/">
+				<div class="textwrap">
+					<h1>dance</h1>
+					<h1>against</h1>
+					<h1>corona</h1>
+				</div>
+			</a>
 		</div>
 	);
 }
