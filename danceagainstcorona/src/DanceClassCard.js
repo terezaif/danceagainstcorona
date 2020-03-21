9e17b9d7878b3c0c0fef41e0440084b46ba3677f
yennycheung <yennycheung@hotmail.com>
2020-03-21 17:13:50 +0100

    Get the correct timestrings

--- danceagainstcorona/src/DanceClassCard.js
+++ danceagainstcorona/src/DanceClassCard.js
@@ -4,10 +4,13 @@ class DanceClassCard extends Component {
 	render() {
 		const danceClass = this.props;
 		const artists = danceClass.artists;
+
+		const timeString = new Date(danceClass.dateTime).toLocaleTimeString().slice(0,-3);
 		const igHandles = artists.map(artist => artist.instagram).join(", ");
+
 		return (
 			<div>
-				{danceClass.dateTime}: {danceClass.danceStyle} with {igHandles}
+				{timeString}: {danceClass.danceStyle} with {igHandles}
 			</div>
 		);
 	}
