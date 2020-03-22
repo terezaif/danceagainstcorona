8a273175a20697584e91d7b5e7f6de11fc94bcdb
Yenny Cheung <cheungyenny@gmail.com>
2020-03-22 17:44:39 +0100

    Handles error case, added links to instagram (#23)

--- danceagainstcorona/src/DanceClassCard.js
+++ danceagainstcorona/src/DanceClassCard.js
@@ -7,11 +7,15 @@ class DanceClassCard extends Component {
 		const danceClass = this.props;
 		const artists = danceClass.artists;
 
-		const happeningAt = new Date(danceClass.dateTime) //.toLocaleTimeString().slice(0,-3);
+		const happeningAt = new Date(danceClass.dateTime);
 		const minutes = happeningAt.getMinutes();
+
+		// If the minutes are single digit, it doesn't have a pretrailing 0, so adding this here
 		const minutePrefix = minutes > 10 ? '' : '0';
 		const timeString = `${happeningAt.getHours()}:${minutePrefix}${minutes}`;
-		const igHandles = artists.map(artist => artist.instagram).join(", ");
+		const igHandles = artists
+			.map(artist => <a href={"https://www.instagram.com/" + artist.instagram.substring(1) +"/"}>{artist.instagram}</a>)
+			.reduce((prev, curr) => [prev, ', ', curr]);
 
 		return (
 			<div className="dance-class-card">
--- danceagainstcorona/src/Loader.js
+++ danceagainstcorona/src/Loader.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './Loader.css';
 
-function Loader() {
+function Loader(props) {
 	return (
 		<div className="loader">
-			<p className="loader-text">Preparing your dance schedule for you... <span role="img" aria-label="hourglass emoji">&#x231b;</span></p>
+			{props.isError ?
+			<p className="loader-text">There is a problem. Our development team is working hard to fix it. Please come back later. <span role="img" aria-label="hourglass emoji">&#x231b;</span></p>:
+			<p className="loader-text">Preparing your dance schedule for you... <span role="img" aria-label="hourglass emoji">&#x231b;</span></p>}
 		</div>
 	);
 }
