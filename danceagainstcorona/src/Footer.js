c3044ebce20b017e7f94f30ace18a9bd21f47001
Yenny Cheung <cheungyenny@gmail.com>
2020-03-22 19:16:15 +0100

    Add google analytics (#26)
    * Handles error case, added links to instagram
    
    * instead of repeating svg, making it to repeating background to improve performance
    
    * Added google analytics, add credts for ladylis

--- danceagainstcorona/src/Footer.js
+++ danceagainstcorona/src/Footer.js
@@ -12,12 +12,13 @@ function Footer() {
 					<p>When you take a class online, we encourage you to support the dancers. <span role="img" aria-label="flexed biceps emoji">&#x1F4AA;</span> Thank you for your help in sustaining a diverse dance scene during the turbulent times.</p>
 				</div>
 				<div className="credits">
-					Powered by&nbsp;
+					<p>This website was inspired by <a href="https://www.instagram.com/ladylis_dhclasshh/">@ladylis_dhclasshh</a> with her creation of <a href="https://www.instagram.com/danceagainstcorona/">@danceagainstcorona</a>.</p>
+					<p>It is powered by&nbsp;
 					<a href="https://airtable.com/">Airtable</a>,&nbsp;
 					<a href="https://www.heroku.com/">Heroku</a>,&nbsp;
 					<a href="https://www.netlify.com/">Netlify</a>,&nbsp;
 					<a href="https://www.python.org/">Python</a> and&nbsp;
-					<a href="https://reactjs.org/">React.js</a>.
+					<a href="https://reactjs.org/">React.js</a>.</p>
 				</div>
 			</div>
 		</div>
